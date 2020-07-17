4e5d22fd0083ed5c6e647b12a8e0c4a4f0a26015
acher1234 <Poltronc01>
2020-07-17 11:33:31 +0200

    commit

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
-  $conn= new mysqli("localhost", "root","","rata");
+function returnAConnexion()
+{
+  $ipAddress = "localhost:8889";
+  $user_name = "Acher";
+  $Password = "Project01";
+  $databaseName = "ProjectDatabase";
+  $conn= new mysqSli($ipAddress,$user_name,$Password,$databaseName);
    
    if ($conn->connect_error){
-	   die("could not connect to the datebase".$conn->connect_error);
+     die("could not connect to the datebase".$conn->connect_error);
+     return "error";
    }
-  
+   return $conn;
+} 
 ?>
